3d87f3f0c69e93201aa119bc24a29d1f15862d8c
Nanfaice <ice.nanfa@gmail.com>
2024-09-12 15:36:24 +0700

    Add files via upload

---
+++ phpbasic/function32.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>w</title>
+</head>
+<body>
+   <?php
+   function addFunction($num1, $num2)
+   
+   um1 + $num2;
+   um;
+   lue =
+</body>
+</html>
